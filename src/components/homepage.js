22aefa78978abb08e5eca37893ab203330c4a952
Fan Duc'k <gerphan94@gmail.com>
2025-08-14 16:54:08 +0700

    111

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,6 +4,15 @@ import DrugInfoModal from "./thongtinthuoc-modal";
 
 function HomePage() {
 
+    const menu = [
+        { id: 'duyet-thuoc', title: 'Duyệt thuốc', link: 'duyet-thuoc' },
+        { id: 'ky-gui-thuoc', title: 'Ký gửi thuốc', link: 'ky-gui-thuoc' },
+    ]
+    const currentPath = window.location.pathname.split('/').pop();
+    const [sltMenu, setSltMenu] = useState(menu[0].id);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
     const [showTThuoc, setShowTThuoc] = useState(false);
     const [sltTrangThai, setSltTrangThai] = useState(-1);
 
@@ -13,8 +22,19 @@ function HomePage() {
                 <div className="flex">
                     {/* Sidebar */}
                     <div className="w-56 h-screen fixed bg-[#2C43A8] text-white p-2">
-                        <div className="mt-20 text-left">
-                            <div className="text-lg font-semibold cursor-pointer bg-[#017BFB] px-10 py-1 rounded">Duyệt thuốc</div>
+                        <div className="p-4">
+                        <img src="/img/talogo.png" alt="Logo" className="w-full h-18 mx-auto" />
+
+                        </div>
+                        <div className="mt-20 text-left space-y-1">
+                            {menu.map((item) => (
+                                <a 
+                                key={item.id} 
+                                className={`text-lg w-full block font-semibold cursor-pointer ${currentPath === item.link ? 'bg-[#017BFB]' : ''} hover:bg-[#017BFB] px-10 py-1 rounded`}
+                                onClick={() => setSltMenu(item.id)}
+                                href={`${item.link}`}
+                                >{item.title}</a>
+                            ))}
                         </div>
 
                     </div>
