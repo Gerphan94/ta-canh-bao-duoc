af54c519700f69388b4501ec89c6c3353c361cd6
Fan Duc'k <gerphan94@gmail.com>
2025-11-13 08:57:37 +0700

    12121

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -24,6 +24,32 @@ function HomePage() {
         // { id: "tuchoi", name: "Từ chối" },
     ]
 
+
+    const khoas = [
+        { id: 0, name: "" },
+        { id: 1, name: "Tủ trực phòng Cấp cứu sàng lọc" },
+        { id: 2, name: "Khoa Nội tổng quát" },
+        { id: 3, name: "Tủ trực Khoa Cấp Cứu" },
+        { id: 4, name: "TT Tiểu Phẫu Nhi" },
+        { id: 5, name: "Khoa Ngoại Tổng Hợp" },
+        { id: 6, name: "Tủ trực Nội Soi bàng quang (pk)" },
+        { id: 7, name: "Khoa Sản" },
+        { id: 8, name: "Khoa CDHA" },
+        { id: 9, name: "Kho vật tư tiêu hao/thuốc lâm sàng IVF" },
+        { id: 10, name: "Khoa Sơ Sinh" },
+        { id: 11, name: "Tủ trực nội soi tiêu hóa" },
+        { id: 12, name: "Tủ trực Phòng tiểu phẫu chỉnh hình" },
+        { id: 13, name: "Khoa Nam Học" },
+        { id: 14, name: "Khoa Nhi" },
+        { id: 15, name: "KHO THUỐC HỒI TỈNH" },
+        { id: 16, name: "Tủ trực Phòng Khám" },
+        { id: 17, name: "Tủ trực Sơ cấp cứu  Nhi" },
+        { id: 18, name: "TT Khoa Kiểm Soát Nhiễm Khuẩn" },
+        { id: 19, name: "Xe E-cart" },
+        { id: 20, name: "Phòng Tiểu phẫu (pk)" }
+    ];
+
+
     return (
         <>
             <div className="h-screen bg-gray-50 overflow-hidden">
@@ -52,8 +78,8 @@ function HomePage() {
                     {/* Filters (non-scrolling header) */}
                     <div className="px-6 mt-2">
                         <div className="bg-white p-4 shadow-sm rounded-md">
-                            <div className="flex flex-wrap items-center gap-4">
-                                <div className="flex items-center gap-2">
+                            <div className="grid grid-cols-4 gap-4">
+                                <div className="flex justify-between items-center gap-2">
                                     <label className="text-sm font-medium">Từ ngày:</label>
                                     <input
                                         type="date"
@@ -63,7 +89,7 @@ function HomePage() {
                                     />
                                 </div>
 
-                                <div className="flex items-center gap-2">
+                                <div className="flex justify-between items-center gap-2">
                                     <label className="text-sm font-medium">Đến ngày:</label>
                                     <input
                                         type="date"
@@ -73,7 +99,7 @@ function HomePage() {
                                     />
                                 </div>
 
-                                <div className="flex gap-4 items-center">
+                                <div className="col-span-2 flex gap-4 items-center">
                                     {trangthais.map((trangthai) => (
                                         <label
                                             key={trangthai.id}
@@ -91,13 +117,29 @@ function HomePage() {
                                     ))}
                                 </div>
 
+                                <input
+                                    type="text"
+                                    className="border outline-none px-2 py-1 rounded"
+                                    placeholder="Nhập PID"
+                                />
+
+                                <select
+                                    className="col-span-2 border outline-none px-2 py-1 rounded"
+                                >
+                                    {khoas.map((khoa) => (
+                                        <option key={khoa.id} value={khoa.id}>
+                                            {khoa.name}
+                                        </option>
+                                    ))}
+                                </select>
                                 <button
                                     type="button"
-                                    className="bg-blue-800 text-white px-6 py-2 rounded text-sm font-medium hover:bg-blue-900"
+                                    className="w-1/2 bg-blue-800 text-white px-6 py-2 rounded text-sm font-medium hover:bg-blue-900"
                                 >
                                     Xem
                                 </button>
                             </div>
+
                         </div>
                     </div>
 
