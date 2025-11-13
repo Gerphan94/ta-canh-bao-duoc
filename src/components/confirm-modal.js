7418ee865a72522ac65d94e77911f0c2f0f436f3
Fan Duc'k <gerphan94@gmail.com>
2025-11-13 10:26:47 +0700

    nhập lý do

--- src/components/confirm-modal.js
+++ src/components/confirm-modal.js
@@ -4,12 +4,28 @@ function ConfirmModal({ setShow, mabn, onClick, confirmType }) {
 
 
 
-    const [lyDo, setLyDo] = useState('');
     const inputRef = useRef(null);
+    const [lydo, setLydo] = useState('');
+    const lydos = [
+        '', 'Sai ICD', "Quá liều lượng", "Sai đường dùng", "Sai cách dùng", "Sai tương tác thuốc", "Sai liều lượng",
+    ]
+    const [sltlydo, setSltlydo] = useState('')
+    const handleAddLydo = () => {
+        if (!sltlydo) return; // không thêm khi trống
+
+        // Nếu lydo đang trống, chỉ set lý do đầu tiên
+        // Nếu đã có rồi, nối thêm bằng dấu ;
+        // Nếu sltlydo đã có trong lydo, khong thêm
+        if (lydo.includes(sltlydo)) return;
+
+        setLydo(prev => prev ? `${prev}; ${sltlydo}` : sltlydo);
+
+        setSltlydo(''); // reset select sau khi thêm
+    };
 
 
     const handleClickTuChoi = () => {
-        if (!lyDo.trim()) {
+        if (!lydo.trim()) {
             // If empty, focus the input
             inputRef.current?.focus();
             return;
@@ -24,20 +40,38 @@ function ConfirmModal({ setShow, mabn, onClick, confirmType }) {
             <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
                 <div className="bg-white rounded-lg shadow-lg max-w-md w-full max-h-[90vh] overflow-y-auto px-6 py-6">
                     <h2 className="text-xl font-bold text-gray-800 mb-4 text-center">Xác nhận {confirmType === 'dongy' ? "đồng ý" : "từ chối"}</h2>
+                    <div className="flex gap-2 items-center py-2">
+                        <select
+                            className="w-full border px-2 py-1 outline-none rounded"
+                            value={sltlydo}
+                            onChange={(e) => setSltlydo(e.target.value)}
+                        >
+                            {lydos.map((lydo, index) => (
+                                <option key={index}>{lydo}</option>
+                            ))}
+                        </select>
+                        <button className=" border bg-blue-500 text-white px-2 py-1 rounded"
+                            onClick={handleAddLydo}
+                        >Thêm</button>
+                    </div>
+
                     <div>
                         <textarea
                             ref={inputRef}
-                            value={lyDo}
-                            onChange={(e) => setLyDo(e.target.value)}
-                            className="w-full border border-gray-300 rounded-md p-2"
+                            value={lydo}
+                            rows="4"
+                            spellCheck="false"
+                            autoComplete="off"
+                            onChange={(e) => setLydo(e.target.value)}
+                            className="w-full border border-gray-300 rounded-md p-2 outline-none"
                             placeholder={`Nhập lý do ${confirmType === 'dongy' ? "đồng ý" : "từ chối"}...`}
                         />
                     </div>
                     <div className="flex justify-center gap-4 pt-4 text-sm">
-                        <button className={` ${confirmType === 'dongy' ? "bg-blue-600 hover:bg-blue-700" :"bg-red-500 hover:bg-red-600"} text-white px-8 py-1.5 rounded`}
+                        <button className={` ${confirmType === 'dongy' ? "bg-blue-600 hover:bg-blue-700" : "bg-red-500 hover:bg-red-600"} text-white px-8 py-1.5 rounded`}
                             onClick={() => handleClickTuChoi()}
                         >
-                         {confirmType === 'dongy' ? "Đồng ý" : "Từ chối"}   
+                            {confirmType === 'dongy' ? "Đồng ý" : "Từ chối"}
                         </button>
                         <button
                             onClick={() => setShow(false)}
